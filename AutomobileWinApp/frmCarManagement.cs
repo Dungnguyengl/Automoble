14f32ad7d71edf96f64d4338afa386e5bdcd8f91
QuyenPN <pnquyen.nvla2.2019@gmail.com>
2023-10-03 08:57:05 +0700

    Activity3 commit

---
+++ AutomobileWinApp/frmCarManagement.cs
@@ -0,0 +1,30 @@
+﻿namespace AutomobileWinApp
+{
+    public partial class frmCarManagement : Form
+    {
+        public frmCarManagement()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmCarManagement_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
